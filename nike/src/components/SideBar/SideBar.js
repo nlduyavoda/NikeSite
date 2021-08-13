d4a7b7c9b9b9a345ee35c3db447e48a62c062fe3
Nguyễn Duy <nguyenleduy1199@gmail.com>
2021-08-13 17:12:52 +0700

    hidden sidebar category

--- nike/src/components/SideBar/SideBar.js
+++ nike/src/components/SideBar/SideBar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
-import { BsChevronCompactDown } from "react-icons/bs";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 function SideBar() {
   const arrSize = [];
+  const [GenderBtn, setGenderBtn] = useState(true);
+  const [SizeBtn, setSizeBtn] = useState(true);
+  const [ColourBtn, setColourBtn] = useState(true);
+  const handleGenderBtn = () => {
+    setGenderBtn(!GenderBtn);
+  };
+  const handleSizeBtn = () => {
+    setSizeBtn(!SizeBtn);
+  };
+  const handleColourBtn = () => {
+    setColourBtn(!ColourBtn);
+  };
   const Colour = [
     { id: 1, color: "red" },
     { id: 2, color: "black" },
@@ -29,72 +41,120 @@ function SideBar() {
   return (
     <div className="SideBar_">
       <div className="SideBar_container">
-      <div className="category">
-        <div className="category_item">Lifestyle</div>
-        <div className="category_item">Running</div>
-        <div className="category_item">Basketball</div>
-        <div className="category_item">Football</div>
-        <div className="category_item">Trainning & Gym</div>
-        <div className="category_item">Jordan</div>
-        <div className="category_item">Skateboarding</div>
-        <div className="category_item">Golf</div>
-        <div className="category_item">Tennis</div>
-        <div className="category_item">Athletics</div>
-        <div className="category_item">Walking</div>
-      </div>
-      <div className="gender">
-        <div className="gender_title">
-          <div className="gender_title_">Gender(1)</div>
-          <div className="gender_title_icon">
-            <BsChevronCompactDown />
+        <div className="category">
+          <div className="category_item">Lifestyle</div>
+          <div className="category_item">Running</div>
+          <div className="category_item">Basketball</div>
+          <div className="category_item">Football</div>
+          <div className="category_item">Trainning & Gym</div>
+          <div className="category_item">Jordan</div>
+          <div className="category_item">Skateboarding</div>
+          <div className="category_item">Golf</div>
+          <div className="category_item">Tennis</div>
+          <div className="category_item">Athletics</div>
+          <div className="category_item">Walking</div>
+        </div>
+        <div className="gender">
+          <div className="gender_title">
+            <div className="gender_title_">Gender(1)</div>
+            <div className="gender_title_icon" onClick={handleGenderBtn}>
+              {GenderBtn === true ? <BsChevronDown /> : <BsChevronUp />}
+            </div>
+          </div>
+          <div
+            className={
+              GenderBtn === true ? "group_checkbox" : "group_checkbox active"
+            }
+          >
+            <div
+              className={
+                GenderBtn === true
+                  ? "gender_checkbox"
+                  : "gender_checkbox active"
+              }
+            >
+              <input type="checkbox" id="myCheck"></input> Men
+            </div>
+            <div
+              className={
+                GenderBtn === true
+                  ? "gender_checkbox"
+                  : "gender_checkbox active"
+              }
+            >
+              <input type="checkbox" id="myCheck"></input> Women
+            </div>
+            <div
+              className={
+                GenderBtn === true
+                  ? "gender_checkbox"
+                  : "gender_checkbox active"
+              }
+            >
+              <input type="checkbox" id="myCheck"></input> Boys
+            </div>
+            <div
+              className={
+                GenderBtn === true
+                  ? "gender_checkbox"
+                  : "gender_checkbox active"
+              }
+            >
+              <input type="checkbox" id="myCheck"></input> Girls
+            </div>
+            <div
+              className={
+                GenderBtn === true
+                  ? "gender_checkbox"
+                  : "gender_checkbox active"
+              }
+            >
+              + More
+            </div>
           </div>
         </div>
-
-        <div className="gender_checkbox">
-          <input type="checkbox" id="myCheck"></input> Men
-        </div>
-        <div className="gender_checkbox">
-          <input type="checkbox" id="myCheck"></input> Women
-        </div>
-        <div className="gender_checkbox">
-          <input type="checkbox" id="myCheck"></input> Boys
-        </div>
-        <div className="gender_checkbox">
-          <input type="checkbox" id="myCheck"></input> Girls
-        </div>
-        <div className="gender_checkbox">+ More</div>
-      </div>
-      <div className="Size">
-        <div className="gender_title">
-          <div className="gender_title_">SIZE</div>
-          <div className="gender_title_icon">
-            <BsChevronCompactDown />
+        <div className="Size">
+          <div className="gender_title">
+            <div className="gender_title_">SIZE</div>
+            <div className="gender_title_icon" onClick={handleSizeBtn}>
+              {SizeBtn === true ? <BsChevronDown /> : <BsChevronUp />}
+            </div>
+          </div>
+          <div
+            className={SizeBtn === true ? "group_size" : "group_size active"}
+          >
+            <div className={SizeBtn === true ? "sizeRow" : "sizeRow active"}>
+              {size}
+            </div>
           </div>
         </div>
-        <div className="sizeRow">{size}</div>
-      </div>
-      {/* //colour */}
-      <div className="Size">
-        <div className="gender_title">
-          <div className="gender_title_">Colour</div>
-          <div className="gender_title_icon">
-            <BsChevronCompactDown />
+        {/* //colour */}
+        <div className="Size">
+          <div className="gender_title">
+            <div className="gender_title_">Colour</div>
+            <div className="gender_title_icon" onClick={handleColourBtn}>
+              {ColourBtn === true ? <BsChevronDown /> : <BsChevronUp />}
+            </div>
+          </div>
+          <div
+            className={
+              ColourBtn === true ? "group_colour" : "group_colour active"
+            }
+          >
+            <div className={ColourBtn === true ? "_colour" : "_colour active"}>
+              {Colour.map((item) => {
+                return (
+                  <>
+                    <div className="container_box">
+                      <div id={item.color} className="colourBox"></div>
+                      <div className="colourText">{item.color}</div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div className="sizeRow">
-          {Colour.map((item) => {
-            return (
-              <>
-                <div className="container_box">
-                  <div id={item.color} className="colourBox"></div>
-                  <div className="colourText">{item.color}</div>
-                </div>
-              </>
-            );
-          })}
-        
-        </div>
-      </div>
       </div>
     </div>
   );
